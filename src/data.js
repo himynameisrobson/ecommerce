993aed13b8c8cfbbc131145a3488108f0cd3aaf9
Robson Elias de Oliveira <himynameisrobson@gmail.com>
2022-02-13 16:16:39 -0300

    add new project

---
+++ src/data.js
@@ -0,0 +1,26 @@
+
+
+const data = {
+    products: [
+      {
+        id: '1',
+        name: 'Charizard VMAX',
+        price: 3400,
+        image: 'https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/SHF/SHF_SV107_R_EN_LG.png',
+      },
+      {
+        id: '2',
+        name: 'Blastoise GX',
+        price: 1400,
+        image: 'https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/UNB/UNB_035_R_EN_LG.png',
+      },
+      {
+        id: '3',
+        name: 'Alakazam EX',
+        price: 1000,
+        image: 'https://limitlesstcg.nyc3.digitaloceanspaces.com/tpci/FCO/FCO_025_R_EN_LG.png',
+      },
+    ],
+  };
+
+  export default data;
